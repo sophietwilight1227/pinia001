1cb933fd3823edba5038301889ae3e47800af6cc
nanai <hanakoizumian0629@yahoo.co.jp>
2025-07-21 12:50:53 +0900

    test3

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/pinia001/', //通常はこっち
-  //base: '/',  //カスタムドメインを使うならこっち？
+  //base: '/pinia001/', //通常はこっち
+  base: '/',  //カスタムドメインを使うならこっち？
 })
